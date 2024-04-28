2b7f345522952885c2d720822c00564a41481103
Tranduy1dol <tmd.iid2004@gmail.com>
2024-04-28 10:49:57 +0700

    fix: security file structure

--- app/src/secure/Contract/compile.js
+++ app/src/secure/Contract/compile.js
@@ -1,8 +1,9 @@
 const solc = require('solc');
-const fs = require('fs');
+const { readFileSync} = require("node:fs");
+// const fs = require('fs');
 
 // Đọc mã Solidity từ file
-const contractCode = fs.readFileSync('Smail.sol', 'utf8');
+const contractCode = readFileSync('Smail.sol', 'utf8');
 
 // Trình biên dịch Solidity
 const input = {
@@ -24,7 +25,7 @@ const input = {
 // Biên dịch contract
 const abi = JSON.parse(solc.compile(JSON.stringify(input))).contracts['Smail.sol']['Smail'].abi;
 const evm = JSON.parse(solc.compile(JSON.stringify(input))).contracts['Smail.sol']['Smail'].evm;
-fs.writeFileSync('abi.json', JSON.stringify(abi), 'utf8');
+// fs.writeFileSync('abi.json', JSON.stringify(abi), 'utf8');
 module.exports = { abi, evm };
 
 
